f77dbc4d63fc1b9fcf2bd75f4b088a57b1d47d3e
Akshat Kumar <akshatkumarjha2005@gmail.com>
2026-02-13 22:13:17 +0530

    Very minor additions

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Dashboard() {
-  return <div id="DashBoard_Container"></div>;
+  return (
+    <div id="DashBoard_Container">
+      <img src="/src/assets/icon.svg" id="icon" />
+    </div>
+  );
 }
